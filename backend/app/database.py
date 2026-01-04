d67b7ca9be04063062a4660631316ef49af0a0da
THOUTAM SAHAJA <thoutamsahaja9@gmail.com>
2026-01-04 17:10:39 +0530

    Setup database architecture: SQLModel, User table

---
+++ backend/app/database.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel, create_engine
+
+DATABASE_URL = "sqlite:///./database.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
---
+++ backend/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from backend.app.database import create_db_and_tables
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+@app.get("/")
+def root():
+    return {"message": "API is running"}
